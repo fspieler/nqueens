b985ca94b6a16a8fe94ceb9b4d46c60d26347488
Fred Spieler <fspieler@gmail.com>
2015-12-04 18:08:32 -0500

    Add iterative solution

---
+++ nqueens_iter.cpp
@@ -0,0 +1,71 @@
+#include <debug.h>
+#include <vector>
+#include <array>
+#include <stdio.h>
+#include <cstdlib>
+
+std::array<int,25> cols;
+std::array<int,50> diag_desc;
+std::array<int,50> diag_asc;
+std::vector<int> stack;
+int stack_index = 0;
+int count = 0;
+
+int num_solns(int DIM)
+{
+  int r = 0;
+  stack.push_back(-1);
+outer:
+  while(r >= 0)
+  {
+    if(r == DIM)
+      count++;
+    else
+    {
+      int c = 0;
+      if(stack_index > r)
+      {
+        c = 1+stack[stack_index--];
+        if(stack.size())
+          stack.pop_back();
+      }
+      for(; c < (!r ? (DIM&1) ? DIM/2+1 : DIM/2 : DIM); ++c)
+      {
+        if(DIM&1 && r == 0 && c == DIM/2)
+          count*=2;
+        if(!cols[c] && !diag_desc[DIM-1+r-c] && !diag_asc[r+c])
+        {
+          stack_index++;
+          stack.push_back(c);
+          cols[c] = true;
+          diag_desc[DIM-1+r-c] = true;
+          diag_asc[r+c] = true;
+          r++;
+          goto outer;
+        }
+      }
+    }
+
+    int c = stack[stack_index];
+    --r;
+    cols[c] = false;
+    diag_desc[DIM-1+r-c] = false;
+    diag_asc[r+c] = false;
+  }
+  if(DIM%2==0)
+    count*=2;
+  return count;
+}
+
+int main(int argc,char**argv)
+{
+  if(argc < 2)
+  {
+    printf("Provide single argument n (number of queens on n*n board)\n");
+    return 1;
+  }
+
+  int dim = atoi(argv[1]);
+  printf("%d: %d\n", dim, num_solns(dim));
+  return 0;
+}
