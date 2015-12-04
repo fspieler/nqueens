9b851ffd1d7bc558d4ead20552df8ed870c8fa9b
Fred Spieler <fspieler@gmail.com>
2015-12-04 07:54:42 -0500

    Recursive optimization, minor reorg

--- Makefile
+++ Makefile
@@ -1,18 +1,16 @@
-.PHONY: all run-test clean
+.PHONY: all clean
 
-all: clean run
+all: clean nqueens_recur
 
-run: nqueens
-	@\time -p ./nqueens
 
-nqueens:
-	g++ -O2 -I. -W -Wall -Wextra -std=c++14 main.cpp -o nqueens
+nqueens_recur: nqueens_recur.cpp
+	g++ -O3 -I. -W -Wall -Wextra -std=c++11 nqueens_recur.cpp -o nqueens_recur
 
-run-test: test
-	@\time -p ./test --gtest_color=yes
+#run-test: test
+#	@\time -p ./test --gtest_color=yes
 
-test: ccct_trie.t.cpp
-	g++ -O2 -I. -pthread -W -Wall -Wextra -std=c++14 ccct_trie.internal.t.cpp /usr/local/lib/libgtest.a -o test
+#test: ccct_trie.t.cpp
+#	g++ -O2 -I. -pthread -W -Wall -Wextra -std=c++11 ccct_trie.internal.t.cpp /usr/local/lib/libgtest.a -o test
 
 clean: 
-	rm -f nqueens
+	rm -f nqueens_recur
---
+++ nqueens_recur.cpp
@@ -0,0 +1,45 @@
+#include <vector>
+#include <stdio.h>
+#include <cstdlib>
+
+std::vector<int> cols(25,0);
+std::vector<int> diag_desc(50,0);
+std::vector<int> diag_asc(50,0);
+int count = 0;
+
+int num_solns(int DIM, int r=0)
+{
+  if(r == DIM)
+  {
+    return ++count;
+  }
+  for(int c = 0; c < (!r ? (DIM&1) ? DIM/2+1 : DIM/2 : DIM); ++c)
+  {
+    if(r == 0 && c == DIM/2)
+      count*=2;
+    if(!cols[c] && !diag_desc[DIM-1+r-c] && !diag_asc[r+c])
+    {
+      cols[c] = true;
+      diag_desc[DIM-1+r-c] = true;
+      diag_asc[r+c] = true;
+      num_solns(DIM, r+1);
+      cols[c] = false;
+      diag_desc[DIM-1+r-c] = false;
+      diag_asc[r+c] = false;
+    }
+  }
+  return count;
+}
+
+int main(int argc,char**argv)
+{
+  if(argc < 2)
+  {
+    printf("Provide single argument n (number of queens on n*n board)\n");
+    return 1;
+  }
+
+  int dim = atoi(argv[1]);
+  printf("%d: %d\n", dim, num_solns(dim));
+  return 0;
+}
