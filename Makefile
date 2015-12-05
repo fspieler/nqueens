28fcf5c108dafdadbb6b71b992f93a381e869d07
Fred Spieler <fred@fredspieler.com>
2015-12-05 01:49:30 -0500

    Add multi-threaded version
    This is the first working multi-threaded version, using a
    divide-and-conquer approach. Divides the columns on the first row
    evenly amongst several threads.

--- Makefile
+++ Makefile
@@ -1,24 +1,36 @@
-.PHONY: all clean run_test_iter run_test_recur
+.PHONY: all clean run_test_iter run_test_recur run_test_threads
 
 all: clean nqueens_recur nqueens_iter
 
-nqueens_recur: nqueens_recur.cpp main.cpp
-	g++ -O3 -I. -W -Wall -Wextra -std=c++14 main.cpp nqueens_recur.cpp -o nqueens_recur
+clean: 
+	rm -f nqueens_recur nqueens_iter nqueens_threads test_iter test_recur test_threads
 
+# iterative
 nqueens_iter: nqueens_iter.cpp main.cpp
 	g++ -O3 -I. -W -Wall -Wextra -std=c++14 main.cpp nqueens_iter.cpp -o nqueens_iter
-
-run_test_iter: test_iter
-	./test_iter --gtest_filter=nqueens.n_less*
-
-run_test_recur: test_recur 
-	./test_recur --gtest_filter=nqueens.n_less*
-
-test_recur: tests.cpp nqueens_recur.cpp
-	g++ -O3 -I. -pthread -W -Wall -Wextra -std=c++14 tests.cpp nqueens_recur.cpp -lgtest_main -o test_recur
 
 test_iter: tests.cpp nqueens_iter.cpp
 	g++ -O3 -I. -pthread -W -Wall -Wextra -std=c++14 tests.cpp nqueens_iter.cpp -lgtest_main -o test_iter
 
-clean: 
-	rm -f nqueens_recur nqueens_iter test_iter test_recur
+run_test_iter: test_iter
+	./test_iter --gtest_filter=nqueens.n_less*
+
+# recursive
+nqueens_recur: nqueens_recur.cpp main.cpp
+	g++ -O3 -I. -W -Wall -Wextra -std=c++14 main.cpp nqueens_recur.cpp -o nqueens_recur
+
+test_recur: tests.cpp nqueens_recur.cpp
+	g++ -O3 -I. -pthread -W -Wall -Wextra -std=c++14 tests.cpp nqueens_recur.cpp -lgtest_main -o test_recur
+
+run_test_recur: test_recur 
+	./test_recur --gtest_filter=nqueens.n_less*
+
+# multi-threaded
+nqueens_threads: nqueens_threads.cpp main.cpp
+	g++ -O3 -I. -pthread -W -Wall -Wextra -std=c++14 main.cpp nqueens_threads.cpp -o nqueens_threads
+
+test_threads: tests.cpp nqueens_threads.cpp
+	g++ -O3 -I. -pthread -W -Wall -Wextra -std=c++14 tests.cpp nqueens_threads.cpp -lgtest_main -o test_threads
+
+run_test_threads: test_threads 
+	./test_threads --gtest_filter=nqueens.n_less*
---
+++ nqueens_threads.cpp
@@ -0,0 +1,110 @@
+#include "debug.h"
+#include <array>
+#include <vector>
+#include <mutex>
+#include <thread>
+
+#include "nqueens.h"
+
+void reset() {}
+
+class Solver
+{
+  std::array<int,25> cols;
+  std::array<int,50> diag_desc;
+  std::array<int,50> diag_asc;
+  std::array<int,25> stack;
+  int stack_index = 0;
+  int count = 0;
+public:
+  int run(int dim, int r, int c_beg, int c_end);
+  Solver() = default;
+  Solver(const Solver&) = default;
+  Solver(Solver&&) = delete;
+};
+
+int nqueens(int DIM)
+{
+  if(DIM <= 14 && false)
+  {
+    int ret = Solver().run(DIM, 0, 0, DIM/2) * 2; 
+    if(DIM % 2) //odd
+      ret += Solver().run(DIM, 0, DIM/2, DIM/2+1);
+    return ret;
+  } else
+  {
+    std::mutex m;
+    int count = 0;
+    int cols_per_thread = 2;
+    auto compute = [&](auto r, auto c_beg, auto c_end, bool double_it)->void {
+      int temp = Solver().run(DIM, r, c_beg, c_end);
+      if(double_it)
+        temp *= 2;
+      m.lock();
+      count += temp;
+      m.unlock();
+    };
+
+    std::vector<std::thread> threads;
+    if(DIM % 2) //odd
+    {
+      threads.emplace_back(compute, 0, DIM/2, DIM/2+1, false);
+    }
+    int i = 0;
+    for(;;)
+    {
+      threads.emplace_back( compute, 0, i*cols_per_thread, std::min(DIM/2,(i+1)*cols_per_thread), true);
+      if((i+1) * cols_per_thread > DIM / 2 )
+        break;
+      i++;
+    }
+
+
+    for(std::thread& t : threads)
+    {
+      t.join();
+    }
+
+    return count;
+  }
+}
+
+int Solver::run(int DIM, int r, int c_beg, int c_end)
+{
+  const int initial_r = r;
+  c_end = std::min(c_end,DIM);
+outer:
+  while(r >= initial_r)
+  {
+    if(r == DIM)
+      count++;
+    else
+    {
+      int c = (initial_r==r ? c_beg : 0);
+      if(stack_index > r)
+      {
+        c = 1+stack[stack_index--];
+      }
+      for(; c < (initial_r==r ? c_end : DIM ); ++c)
+      {
+        if(!cols[c] && !diag_desc[DIM-1+r-c] && !diag_asc[r+c])
+        {
+          stack_index++;
+          stack[stack_index] = c;
+          cols[c] = true;
+          diag_desc[DIM-1+r-c] = true;
+          diag_asc[r+c] = true;
+          r++;
+          goto outer;
+        }
+      }
+    }
+
+    int c = stack[stack_index];
+    r--;
+    cols[c] = false;
+    diag_desc[DIM-1+r-c] = false;
+    diag_asc[r+c] = false;
+  }
+  return count;
+}
